9e8f8c80662ed66dbfb95883df8f294b0c06f8c1
unknown <ruhelakunal1997@gmail.com>
2019-08-19 11:34:18 +0500

    view page to see the database entry

---
+++ view.php
@@ -0,0 +1,24 @@
+<?php
+// session_start();
+// echo "welcome"." ". $_SESSION['uname'];
+// $userprofile=$_SESSION['uname'];
+// if($userprofile==true)
+// {
+
+// }
+// else
+// {
+//     header('location:adminpage.php');
+// }
+require_once('conn.php');
+$uname=$_GET['uname'];
+$q="SELECT * FROM users where uname='$uname'";
+$result=mysqli_query($conn,$q);
+$row=mysqli_fetch_assoc($result);
+foreach($row as $ke=> $val)
+{
+   echo $ke.": ".$val."<br>";
+}
+
+?>
+<a href="logout.php">logout</a>
